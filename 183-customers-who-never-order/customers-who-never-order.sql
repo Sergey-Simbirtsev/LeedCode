f63891b4cab22230d6ec14f21be43842c109e2ca
Sergey <cergejsimba@gmail.com>
2026-06-17 18:35:58 +0300

    Time: 232 ms (22.80%) | Memory: 0B (100.00%) - LeetSync

--- 183-customers-who-never-order/customers-who-never-order.sql
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -1,3 +1,3 @@
-SELECT o1.name AS Customers 
-FROM (Customers AS o1 LEFT JOIN Orders AS o2 ON o1.id = o2.customerId )
-WHERE o2.id IS NULL
+SELECT Customers.name AS Customers 
+FROM Customers LEFT JOIN Orders ON Customers.id = Orders.customerId 
+WHERE Orders.id IS NULL
