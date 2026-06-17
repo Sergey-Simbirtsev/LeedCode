f3feacf945dd1c919f3e81402e8fda2c8d054f70
Sergey <cergejsimba@gmail.com>
2026-06-17 16:00:47 +0300

    Time: 242 ms (73.95%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,5 +1,5 @@
-SELECT  Person.firstName, 
-        Person.lastName, 
-        Address.city, 
-        Address.state 
+SELECT  Person.firstName AS firstName, 
+        Person.lastName AS lastName, 
+        Address.city AS city, 
+        Address.state AS state
 FROM Person LEFT JOIN Address ON Person.personId  = Address.personId 
